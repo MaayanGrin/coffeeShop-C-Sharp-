9622028a619d8033afddfb23f6a139782e7aa9d4
MaayanGrin <45363704+MaayanGrin@users.noreply.github.com>
2018-11-26 18:40:23 +0200

    Add files via upload

---
+++ App_Code/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Order
+/// </summary>
+public class Order
+{
+
+    public string Name { get; set; }
+    public string username { get; set; }
+    public int Price { get; set; }
+    public string date { get; set; }
+    public int Amount { get; set; }
+    
+
+    
+        public Order()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
---
+++ App_Code/item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Coffee
+/// </summary>
+public class item
+{
+
+    
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Type { get; set; }
+
+
+public item()
+    {
+        
+    }
+}
